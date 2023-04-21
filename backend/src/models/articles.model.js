40e8a9accecff0ffbfe31476468a9bd99efda300
walwizard <goneisgone@hotmail.com>
2023-04-21 09:21:16 -0300

    Wikipedoa clone - backend, img url

--- backend/src/models/articles.model.js
+++ backend/src/models/articles.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Articles = mongoose.Schema({
     title: {type: String, required: true},
     article: {type: String, required: true},
+    imgUrl: {type: String, required: true},
 })
 
 module.exports = mongoose.model('articles', Articles);
