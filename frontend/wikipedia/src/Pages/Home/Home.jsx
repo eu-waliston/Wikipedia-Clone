a72f80fc58dacaa763bb498acbbe1d961bce6382
Waliston <goneisgone@hotmail.com>
2023-06-29 00:03:26 -0300

    Wikipedia Clone - Update -> Home Page

--- frontend/wikipedia/src/Pages/Home/Home.jsx
+++ frontend/wikipedia/src/Pages/Home/Home.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import "./Home.css";
+/**
+ * ---- TODO 3 Paginas
+ * Login
+ * Register
+ * Recover
+ */
 
 const Home = () => {
-    return (
-        <div className="home__page">
-            <div className="home__itens">
-                <img className="W" src="./Images/Wikipedia_affiliative_mark.png" alt="wiki logo" />
-                <h1>wikipedia</h1>
-                <p>the free <span className="blue__link" >encyclopedia</span> that <span className="blue__link">anyone</span> can edit.</p>
-                <p><span className="blue__link">6,646,428</span> Articles in English</p>
+  return (
+    <div className="home__page">
+      <div className="home__itens">
+        <img
+          className="W"
+          src="./Images/Wikipedia_affiliative_mark.png"
+          alt="wiki logo"
+        />
+        <h1>wikipedia</h1>
+        <p>
+          the free <span className="blue__link">encyclopedia</span> that{" "}
+          <span className="blue__link">anyone</span> can edit.
+        </p>
+        <p>
+          <span className="blue__link">6,646,428</span> Articles in English
+        </p>
 
-                <button className="bnt_articles">Read Articles</button>
-            </div>
-        </div>
-    )
-}
+        <button className="bnt_articles">Read Articles</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
