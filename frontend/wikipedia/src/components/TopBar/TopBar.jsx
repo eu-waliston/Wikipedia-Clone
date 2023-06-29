b880cdbff264eb8ed936bf06c5a5af49a00e0a20
Waliston <goneisgone@hotmail.com>
2023-06-28 23:13:47 -0300

    Wikipedia Clone - Update -> top bar

--- frontend/wikipedia/src/components/TopBar/TopBar.jsx
+++ frontend/wikipedia/src/components/TopBar/TopBar.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import "./TopBar.css";
+// import {Link} from "react-router-dom"
+import { FaUser } from "react-icons/fa";
 
 const TopBar = () => {
-    return (
-        <div className="top__bar">
-            <div className="topbar__itens">
-                <div className="talk__cont">
-                    <a href="/">Talk</a>
-                    <a href="/">Contribuitions</a>
-                </div>
-
-                <div className="login__register">
-                    <a href="/">create account</a>
-                    <span className="separator">/</span>
-                    <a href="/">log in</a>
-
-                    <div>
-                        <input type="text" placeholder="Enter you search..." className="search"/>
-                        <i class="bi bi-search"></i>
-                    </div>
-                </div>
-
-            </div>
+  return (
+    <div className="top__bar">
+      <div className="topbar__itens">
+        <div className="search__component">
+          <input
+            type="text"
+            placeholder="Enter you search..."
+            className="search"
+          />
         </div>
-    )
-}
+
+        <div className="talk__cont">
+          <a href="/">Talk</a>
+          <a href="/">Contribuitions</a>
+        </div>
+
+        <div>
+          <FaUser className="user" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TopBar;
