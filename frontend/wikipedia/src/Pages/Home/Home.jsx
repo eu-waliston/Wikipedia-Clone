e37609f82a75a075720e622462f05335208fa24d
Waliston <goneisgone@hotmail.com>
2023-06-28 23:42:44 -0300

    Wikipedia Clone - Update -> Home Page

--- frontend/wikipedia/src/Pages/Home/Home.jsx
+++ frontend/wikipedia/src/Pages/Home/Home.jsx
@@ -5,10 +5,12 @@ const Home = () => {
     return (
         <div className="home__page">
             <div className="home__itens">
-                <img className="W" src={require("./Wikipedia_affiliative_mark.png")} alt="wiki" />
+                <img className="W" src="./Images/Wikipedia_affiliative_mark.png" alt="wiki logo" />
                 <h1>wikipedia</h1>
                 <p>the free <span className="blue__link" >encyclopedia</span> that <span className="blue__link">anyone</span> can edit.</p>
                 <p><span className="blue__link">6,646,428</span> Articles in English</p>
+
+                <button className="bnt_articles">Read Articles</button>
             </div>
         </div>
     )
